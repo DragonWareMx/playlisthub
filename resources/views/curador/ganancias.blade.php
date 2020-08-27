394b0fa6b2897492ee3f39f8f5c43abc27d1e041
agussolorzano <joseagustinsolorzano@gmail.com>
2020-08-27 17:20:24 -0500

    es poco pero es trabajo honesto
    inserte meme

---
+++ resources/views/curador/ganancias.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.menuGestor')
+
+@section('importOwl')
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/css/O.css">
+@endsection
+
+@section('menu')
+    Ganancias
+@endsection
+
+@section('contenido')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/favoritos', 'OController@favoritos')->name('favoritos');
 Route::get('/campanas', 'OController@campanas')->name('campanas');
 
+//Rutas CURADOR
+Route::get('/ranking', function () {
+    return view('curador.ranking');
+});
+Route::get('/ganancias', function () {
+    return view('curador.ganancias');
+});
+Route::get('/playlists', function () {
+    return view('curador.playlists');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
