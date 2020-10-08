7e59aedc0d3c09173cc1b808b1123dc504e22fbc
FernandoGASA <66817120+FernandoGASA@users.noreply.github.com>
2020-10-08 17:31:29 -0500

    actualizacion reviews

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -515,6 +515,32 @@ class ReviewController extends Controller
                         
                             $camp->save();
 
+                            //se le suma el dinero al curador
+                            switch($camp->level){
+                                case 1:
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    break;
+                                case 4:
+                                    break;
+                                case 5:
+                                    break;
+                                case 6:
+                                    break;
+                                case 7:
+                                    break;
+                                case 8:
+                                    break;
+                                case 9:
+                                    break;
+                                case 10:
+                                    break;
+                                case 11:
+                                    break;
+                            }
+
                             session()->flash('success',true);
                         });
                     }
