72fc88b0377a09f57eac8100f0ff4089ec619ed3
FernandoGASA <66817120+FernandoGASA@users.noreply.github.com>
2020-10-18 00:51:32 -0500

    bug arreglado de reviews :v

--- resources/views/reviews/reviews_pendientes.blade.php
+++ resources/views/reviews/reviews_pendientes.blade.php
@@ -4,7 +4,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="/css/O.css">
     <link rel="stylesheet" type="text/css" href="/css/L.css">
-    <link rel="stylesheet" type="text/css" href="/css/reviews.css">
+    <link rel="stylesheet" type="text/css" href="/css/Reviews.css">
     <link rel="stylesheet" type="text/css" href="/css/perfilMusico.css">
 @endsection
 
