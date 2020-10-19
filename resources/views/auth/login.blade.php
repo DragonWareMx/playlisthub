aab31dfa71beaf3519473047e5a2179344f7453a
agussolorzano <joseagustinsolorzano@gmail.com>
2020-10-19 16:46:16 -0500

    De nada
    De nada

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <title>Iniciar Sesión</title>
     <link rel="stylesheet" href="{{ asset('/css/L.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/L.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/new.css') }}">
     <link rel="icon" href="{{asset('/img/logos/ico-playlist.png')}}" type="image/icon type">
 </head>
 <body style="background-color: #f5f5f5">
