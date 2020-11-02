bfb4e57f5bef3107f11ffbdd529b6cfbd412f818
agussolorzano <joseagustinsolorzano@gmail.com>
2020-11-02 10:02:03 -0600

    el return 0
    ya

--- app/User.php
+++ app/User.php
@@ -46,7 +46,7 @@ class User extends Authenticatable
             return Crypt::decryptString($value);
         }catch(\Exception $e){
             //mandar correo
-            return $value;
+            return '0';
         }
     }
 
